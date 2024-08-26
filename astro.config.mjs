a2b8ff2bc8f2b3d210be1d8f021afe607de1dbb3
Marvin Becker <marvinbecker@derwebcoder.de>
2024-08-26 22:35:12 +0200

    Fix auth redirect issue and enforce trailing slash

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import react from "@astrojs/react";
 export default defineConfig({
 	site: "https://derwebcoder.github.io",
 	base: "sparktag",
+	trailingSlash: "always",
 	integrations: [
 		tailwind({
 			// disabling this because we have the src/styles/global.css file including the base styles,

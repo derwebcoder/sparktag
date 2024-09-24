f48629559b45d9d80c7f69c4ea934acc91be5d96
Marvin Becker <marvinbecker@derwebcoder.de>
2024-09-24 22:36:01 +0200

    pip is working 🎉

--- src/application/SparkInput/SparkInput.tsx
+++ src/application/SparkInput/SparkInput.tsx
@@ -3,48 +3,70 @@ import { sparkService } from "../../scripts/db/SparkService";
 import { TextInput } from "../../ui/components/TextInput/TextInput";
 import { ArrowRightStartOnBox } from "../../ui/icons/ArrowRightStartOnBox";
 import { ArrowRightEndOnBox } from "../../ui/icons/ArrowRightEndOnBox";
+import { createPortal } from "react-dom";
 
 export const SparkInput = () => {
-	const ref = useRef(null);
-	const [attachedTo, setAttachedTo] = useState<"app" | "pip">("app");
+	const [pipWindow, setPipWindow] = useState<Window | undefined>();
 	const handleSubmit = (plainText: string, html: string) => {
 		sparkService.addSpark(plainText, html);
 	};
 
 	const renderPIP = async () => {
-		if (!ref.current) {
-			return;
-		}
-
 		if (window.documentPictureInPicture?.window) {
 			console.log("return");
 			return;
 		}
 
-		const pipWindow = await window.documentPictureInPicture?.requestWindow({
+		const pip = await window.documentPictureInPicture?.requestWindow({
 			width: 500,
 			height: 250,
 		});
-		if (!pipWindow) {
+		if (!pip) {
 			return;
 		}
 
-		pipWindow.document.body.append(ref.current);
-		setAttachedTo("pip");
+		pip.addEventListener("pagehide", (event) => {
+			setPipWindow(undefined);
+		});
+
+		// copy all existing styles because without would be boring
+		const allStyles = [...document.styleSheets];
+		for (const styles of allStyles) {
+			try {
+				const stylesheet = [...styles.cssRules]
+					.map((rule) => rule.cssText)
+					.join(" ");
+				const style = document.createElement("style");
+				style.textContent = stylesheet;
+				pip.document.head.appendChild(style);
+			} catch (e) {
+				if (!styles.href) {
+					continue;
+				}
+				const link = document.createElement("link");
+				link.rel = "stylesheet";
+				link.type = styles.type;
+				link.media = styles.media.toString();
+				link.href = styles.href;
+				pip.document.head.appendChild(link);
+			}
+		}
+
+		setPipWindow(pip);
 	};
 
 	const renderApp = () => {
-		setAttachedTo("app");
+		// setAttachedTo("app");
+		pipWindow?.close();
 	};
 
 	return (
-		<div
-			ref={ref}
-			className="flex flex-col gap-2"
-		>
-			<TextInput onSubmit={handleSubmit} />
-			<div className="flex justify-end px-3">
-				{attachedTo === "app" ? (
+		<>
+			<div className="flex flex-col gap-2">
+				<div className="min-h-32">
+					<TextInput onSubmit={handleSubmit} />
+				</div>
+				<div className="flex justify-end px-3">
 					<button
 						type="button"
 						onClick={renderPIP}
@@ -52,16 +74,24 @@ export const SparkInput = () => {
 					>
 						<ArrowRightStartOnBox />
 					</button>
-				) : (
-					<button
-						type="button"
-						onClick={renderApp}
-						title="attach"
-					>
-						<ArrowRightEndOnBox />
-					</button>
-				)}
+				</div>
 			</div>
-		</div>
+			{pipWindow &&
+				createPortal(
+					<div className="flex flex-col gap-2 h-full">
+						<TextInput onSubmit={handleSubmit} />
+						<div className="flex justify-end px-3">
+							<button
+								type="button"
+								onClick={renderApp}
+								title="attach"
+							>
+								<ArrowRightEndOnBox />
+							</button>
+						</div>
+					</div>,
+					pipWindow.document.body,
+				)}
+		</>
 	);
 };

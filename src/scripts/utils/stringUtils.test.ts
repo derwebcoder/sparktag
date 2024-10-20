f04d8e937d9b8d0b275d27147cd4aa695b729088
Marvin Becker <marvinbecker@derwebcoder.de>
2024-10-20 22:38:28 +0200

    Fixing extractTags tests

--- src/scripts/utils/stringUtils.test.ts
+++ src/scripts/utils/stringUtils.test.ts
@@ -3,7 +3,7 @@ import { extractTags, parseSpark, stringToHue } from "./stringUtils";
 describe("extractTags", () => {
 	test("extracts one tag from only tag input", () => {
 		const html =
-			'<p><span style="--tag-color: 135" data-type="tag" class="tag" data-id="world">#world</span></p>';
+			'<p><span style="--tag-color: 135" data-type="tags" class="tag" data-id="world">#world</span></p>';
 		const expectedTags: string[] = ["world"];
 
 		const tags = extractTags(html);
@@ -23,7 +23,7 @@ describe("extractTags", () => {
 
 	test("extracts multiple tags from only tag input", () => {
 		const html =
-			'<p><span style="--tag-color: 135" data-type="tag" class="tag" data-id="phone">#phone</span> <span style="--tag-color: 1" data-type="tag" class="tag" data-id="number">#thiswillbeignoredandonlydataidwillbeparsed</span></p>';
+			'<p><span style="--tag-color: 135" data-type="tags" class="tag" data-id="phone">#phone</span> <span style="--tag-color: 1" data-type="tags" class="tag" data-id="number">#thiswillbeignoredandonlydataidwillbeparsed</span></p>';
 		const expectedTags: string[] = ["phone", "number"];
 
 		const tags = extractTags(html);
@@ -33,7 +33,7 @@ describe("extractTags", () => {
 
 	test("extracts multiple tags from mixed input", () => {
 		const html =
-			'<p>Fun <span style="--tag-color: 0" data-type="tag" class="tag" data-id="scissor">#scissor</span> #utilitiesNotARealTag <span style="--tag-color: 22" data-type="tag" class="tag" data-id="music">#music</span> life <span style="--tag-color: 444" data-type="tag" class="tag" data-id="attention">#attention</span> </p>';
+			'<p>Fun <span style="--tag-color: 0" data-type="tags" class="tag" data-id="scissor">#scissor</span> #utilitiesNotARealTag <span style="--tag-color: 22" data-type="tags" class="tag" data-id="music">#music</span> life <span style="--tag-color: 444" data-type="tags" class="tag" data-id="attention">#attention</span> </p>';
 		const expectedTags: string[] = ["scissor", "music", "attention"];
 
 		const tags = extractTags(html);
@@ -43,7 +43,7 @@ describe("extractTags", () => {
 
 	test("returns only lowercase tags", () => {
 		const html =
-			'<p><span style="--tag-color: 135" data-type="tag" class="tag" data-id="FreedomEnergy">#FreedomEnergy</span></p>';
+			'<p><span style="--tag-color: 135" data-type="tags" class="tag" data-id="FreedomEnergy">#FreedomEnergy</span></p>';
 		const expectedTags: string[] = ["freedomenergy"];
 
 		const tags = extractTags(html);
@@ -56,13 +56,13 @@ describe("parseSpark", () => {
 	test("returns the correct prefixTags and remainingTags", () => {
 		const content = "#tag1 #tag2 I am great #tag3";
 		const html =
-			'"<p><span style="--tag-color: 135" data-type="tag" class="tag" data-id="tag1">#tag1</span> <span style="--tag-color: 140" data-type="tag" class="tag" data-id="tag2">#tag2</span> I am great <span style="--tag-color: 145" data-type="tag" class="tag" data-id="tag3">#tag3</span></p>"';
+			'"<p><span style="--tag-color: 135" data-type="tags" class="tag" data-id="tag1">#tag1</span> <span style="--tag-color: 140" data-type="tags" class="tag" data-id="tag2">#tag2</span> I am great <span style="--tag-color: 145" data-type="tags" class="tag" data-id="tag3">#tag3</span></p>"';
 		const expectedPrefixTags = ["tag1", "tag2"];
-		const expectedPrefixTagsHtml = `<span style="--tag-color: 135" data-type="tag" class="tag" data-id="tag1">#tag1</span> <span style="--tag-color: 140" data-type="tag" class="tag" data-id="tag2">#tag2</span> `;
+		const expectedPrefixTagsHtml = `<span style="--tag-color: 135" data-type="tags" class="tag" data-id="tag1">#tag1</span> <span style="--tag-color: 140" data-type="tags" class="tag" data-id="tag2">#tag2</span> `;
 		const expectedTags = ["tag1", "tag2", "tag3"];
 		const expectStrippedPlainText = "I am great #tag3";
 		const expectStrippedHtml =
-			'"<p>I am great <span style="--tag-color: 145" data-type="tag" class="tag" data-id="tag3">#tag3</span></p>"';
+			'"<p>I am great <span style="--tag-color: 145" data-type="tags" class="tag" data-id="tag3">#tag3</span></p>"';
 
 		const {
 			prefixTags,
@@ -132,13 +132,13 @@ describe("parseSpark", () => {
 	test("returns empty prefixTags and correct remainingTags when there are no prefix tags and one or more remaining tags", () => {
 		const content = "Hello world #tag1 #tag2 #tag3";
 		const html =
-			'<p>Hello world <span style="--tag-color: 135" data-type="tag" class="tag" data-id="tag1">#tag1</span> <span style="--tag-color: 140" data-type="tag" class="tag" data-id="tag2">#tag2</span> <span style="--tag-color: 145" data-type="tag" class="tag" data-id="tag3">#tag3</span></p>';
+			'<p>Hello world <span style="--tag-color: 135" data-type="tags" class="tag" data-id="tag1">#tag1</span> <span style="--tag-color: 140" data-type="tags" class="tag" data-id="tag2">#tag2</span> <span style="--tag-color: 145" data-type="tags" class="tag" data-id="tag3">#tag3</span></p>';
 		const expectedPrefixTags: string[] = [];
 		const expectedPrefixTagsHtml = "";
 		const expectedTags = ["tag1", "tag2", "tag3"];
 		const expectStrippedPlainText = "Hello world #tag1 #tag2 #tag3";
 		const expectStrippedHtml =
-			'<p>Hello world <span style="--tag-color: 135" data-type="tag" class="tag" data-id="tag1">#tag1</span> <span style="--tag-color: 140" data-type="tag" class="tag" data-id="tag2">#tag2</span> <span style="--tag-color: 145" data-type="tag" class="tag" data-id="tag3">#tag3</span></p>';
+			'<p>Hello world <span style="--tag-color: 135" data-type="tags" class="tag" data-id="tag1">#tag1</span> <span style="--tag-color: 140" data-type="tags" class="tag" data-id="tag2">#tag2</span> <span style="--tag-color: 145" data-type="tags" class="tag" data-id="tag3">#tag3</span></p>';
 
 		const {
 			prefixTags,
@@ -158,14 +158,14 @@ describe("parseSpark", () => {
 	test("returns three prefix tags", () => {
 		const content = "#tag1 #tag2 #tag3";
 		const html =
-			'<p><span style="--tag-color: 135" data-type="tag" class="tag" data-id="tag1">#tag1</span> <span style="--tag-color: 140" data-type="tag" class="tag" data-id="tag2">#tag2</span> <span style="--tag-color: 145" data-type="tag" class="tag" data-id="tag3">#tag3</span></p>';
+			'<p><span style="--tag-color: 135" data-type="tags" class="tag" data-id="tag1">#tag1</span> <span style="--tag-color: 140" data-type="tags" class="tag" data-id="tag2">#tag2</span> <span style="--tag-color: 145" data-type="tags" class="tag" data-id="tag3">#tag3</span></p>';
 		const expectedPrefixTags: string[] = ["tag1", "tag2", "tag3"];
 		const expectedPrefixTagsHtml =
-			'<span style="--tag-color: 135" data-type="tag" class="tag" data-id="tag1">#tag1</span> <span style="--tag-color: 140" data-type="tag" class="tag" data-id="tag2">#tag2</span> <span style="--tag-color: 145" data-type="tag" class="tag" data-id="tag3">#tag3</span> ';
+			'<span style="--tag-color: 135" data-type="tags" class="tag" data-id="tag1">#tag1</span> <span style="--tag-color: 140" data-type="tags" class="tag" data-id="tag2">#tag2</span> <span style="--tag-color: 145" data-type="tags" class="tag" data-id="tag3">#tag3</span> ';
 		const expectedTags = ["tag1", "tag2", "tag3"];
 		const expectStrippedPlainText = "";
 		const expectStrippedHtml =
-			'<p><span style="--tag-color: 135" data-type="tag" class="tag" data-id="tag1">#tag1</span> <span style="--tag-color: 140" data-type="tag" class="tag" data-id="tag2">#tag2</span> <span style="--tag-color: 145" data-type="tag" class="tag" data-id="tag3">#tag3</span></p>';
+			'<p><span style="--tag-color: 135" data-type="tags" class="tag" data-id="tag1">#tag1</span> <span style="--tag-color: 140" data-type="tags" class="tag" data-id="tag2">#tag2</span> <span style="--tag-color: 145" data-type="tags" class="tag" data-id="tag3">#tag3</span></p>';
 
 		const {
 			prefixTags,

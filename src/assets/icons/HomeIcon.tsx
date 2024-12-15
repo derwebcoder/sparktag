2d7ede486b14803af037e331008939fefc4c7a60
Marvin Becker <marvinbecker@derwebcoder.de>
2024-12-15 19:55:42 +0100

    Fix svg props

--- src/assets/icons/HomeIcon.tsx
+++ src/assets/icons/HomeIcon.tsx
@@ -13,14 +13,14 @@ export const HomeIcon = (props: IconProps) => {
 			viewBox="0 0 24 24"
 			strokeWidth={1.5}
 			stroke="currentColor"
-			stroke-linejoin="round"
+			strokeLinejoin="round"
 			className={
 				overrideDefaultClassName ? className : `size-5 ${className}`
 			}
 			role="presentation"
 		>
-			<path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8"/>
-			<path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z"/>
+			<path d="M15 21v-8a1 1 0 0 0-1-1h-4a1 1 0 0 0-1 1v8" />
+			<path d="M3 10a2 2 0 0 1 .709-1.528l7-5.999a2 2 0 0 1 2.582 0l7 5.999A2 2 0 0 1 21 10v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2z" />
 		</svg>
 	);
 };
--- src/assets/icons/ListTodoIcon.tsx
+++ src/assets/icons/ListTodoIcon.tsx
@@ -13,17 +13,23 @@ export const ListTodoIcon = (props: IconProps) => {
 			viewBox="0 0 24 24"
 			strokeWidth={1.5}
 			stroke="currentColor"
-			stroke-linejoin="round"
+			strokeLinejoin="round"
 			className={
 				overrideDefaultClassName ? className : `size-5 ${className}`
 			}
 			role="presentation"
 		>
-			<rect x="3" y="5" width="6" height="6" rx="1"/>
-			<path d="m3 17 2 2 4-4"/>
-			<path d="M13 6h8"/>
-			<path d="M13 12h8"/>
-			<path d="M13 18h8"/>
+			<rect
+				x="3"
+				y="5"
+				width="6"
+				height="6"
+				rx="1"
+			/>
+			<path d="m3 17 2 2 4-4" />
+			<path d="M13 6h8" />
+			<path d="M13 12h8" />
+			<path d="M13 18h8" />
 		</svg>
 	);
 };
